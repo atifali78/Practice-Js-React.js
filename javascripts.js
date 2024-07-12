d2c282300ea99c310503644f2fe11351d845d412
atifali78 <atif7803320@gmail.com>
2024-07-12 16:23:39 +0500

    check if else statement

--- javascripts.js
+++ javascripts.js
@@ -17,19 +17,24 @@
 
 // // }
 // // let practice
-    
+
 // let a = 1;
 //  a = 3
 // console.log("again dclaired the veriable", a)
 
 // // veriable practice
 
-var b = 3;
-{
-    var b = 4;
-    var b = 5;
+// var b = 3;
+// {
+//     var b = 4;
+//     var b = 5;
 
-    console.log("variable declaration",b)
-}
-
-
+//     console.log("variable declaration",b)
+// }
+// ----------------------------------------------------------------------------------------
+//   check a number that is divide by 2 or not
+// 7-12-2024
+let number = prompt("Input a number");
+if (number % 2 == 0) {
+  console.log("This number is divided by 2 = ", number);
+} else console.log("your number is not divided by 2", number);
