750b8425bd14966ee6aca8622dcb6fe735785b54
atifali78 <atif7803320@gmail.com>
2024-07-19 20:47:19 +0500

    functions with arrays

--- Functionss.js
+++ Functionss.js
@@ -7,7 +7,7 @@
 //     val >= 90;
 // });
 
-let arr = [12, 180, 90, 150, 160];
-let filteredArr = arr.filter((val) => val > 90);
+let arr = [12, 122, 90, 150, 160];
+let filteredArr = arr.filter((val) => val > 80);
 console.log(filteredArr);
 
