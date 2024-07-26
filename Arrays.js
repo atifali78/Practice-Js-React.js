9eafe7e986943a447145a9672c44abc718853b67
atifali78 <atif7803320@gmail.com>
2024-07-26 12:18:08 +0500

    rest and spread operator

--- Arrays.js
+++ Arrays.js
@@ -1,4 +1,8 @@
-const arr = [1, 2, "Atif", 5, { name: "Ali" },true];
-arr[arr.indexOf(1) > -1 ? arr.indexOf(1):arrays.length] = 5
-console.log(arr)
+const arr = [1, 2, "Atif", 5, { name: "Ali" }, true];
+arr[arr.indexOf(1) > -1 ? arr.indexOf(1) : arrays.length] = 5;
+console.log(arr);
 
+// -----------------------------------------
+// how to create object in js
+// destructuring in array
+// object destructuring important in js
---
+++ objects.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "Ali",
+  age: 20,
+  adress: {
+    city: "New york",
+  },
+  array: [1, 2],
+  sayHeelo: function () {
+    return 0;
+  },
+};
+console.log(person);
