aca40d815a8c9466d61e1a850fa68bf0d7829a9a
atifali78 <atif7803320@gmail.com>
2024-08-01 21:04:12 +0500

    practice on arrow functions

--- javascripts.js
+++ javascripts.js
@@ -17,19 +17,31 @@
 
 // // }
 // // let practice
-    
+
 // let a = 1;
 //  a = 3
 // console.log("again dclaired the veriable", a)
 
 // // veriable practice
 
-var b = 3;
-{
-    var b = 4;
-    var b = 5;
+// var b = 3;
+// {
+//     var b = 4;
+//     var b = 5;
 
-    console.log("variable declaration",b)
-}
-
-
+//     console.log("variable declaration",b)
+// }
+// -------------------------------------------------------------
+// let a = 5;
+// let b = 4;
+// const addNumber = (a, b) => {
+//   console.log("your values is", a, b);
+// };
+// addNumber(a + b);
+// ----------------------------------------------------------------
+// const addNumber = (a, b) => {
+//     console.log("The sum of a and b is", a + b);
+//     return a + b;
+// };
+//     addNumber(3 , 4);
+// -----------------------------------------------------------------
