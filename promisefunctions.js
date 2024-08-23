aa02a538ebf8bd780e7220bb02056bc03d7c97c2
atifali78 <atif7803320@gmail.com>
2024-08-23 21:49:57 +0500

    promise function

--- promisefunctions.js
+++ promisefunctions.js
@@ -1,21 +1,32 @@
-let promise = new Promise((resolve, reject) => {
-  try {
-    let a = 1;
-    let b = 3;
-    let c = a + b;
+// let promise = new Promise((resolve, reject) => {
+//   try {
+//     let a = 1;
+//     let b = 3;
+//     let c = a + b;
 
-    if (c === 3) {
-      // Corrected comparison operator
-      resolve("Your promise is fulfilled");
-    } else {
-      reject("Condition not met");
-    }
-  } catch (error) {
-    reject(error); // Correctly rejecting the error
-  }
-});
+//     if (c === 3) {
+//       // Corrected comparison operator
+//       resolve("Your promise is fulfilled");
+//     } else {
+//       reject("Condition not met");
+//     }
+//   } catch (error) {
+//     reject(error); // Correctly rejecting the error
+//   }
+// });
 
 
-promise
-  .then((message) => console.log(message))
-  .catch((error) => console.log("Error:", error));
+// promise
+//   .then((message) => console.log(message))
+//   .catch((error) => console.log("Error:", error));
+// ------------------------------------------------------------------------------
+
+constPromise =newPromise((resolve, reject)=>{  
+let condition;  
+  
+if(condition is met){  
+resolve('Promise is resolved successfully.');  
+}else{  
+reject('Promise is rejected');  
+}  
+});  
